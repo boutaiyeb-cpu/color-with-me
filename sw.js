987c847643a8e71224834de4008323ee80b2899e
boutaiyeb-cpu <boutaiyeb@gmail.com>
2026-09-25 22:08:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='cwm-v1';
-const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const FILES=['./','./index.html','./manifest.json','./icon-192-1.png','./icon-512-1.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
