1b2c8643ae10e52f7b55c85198c255efeeb9401b
boutaiyeb-cpu <boutaiyeb@gmail.com>
2026-09-25 19:55:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='cwm-v1';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
